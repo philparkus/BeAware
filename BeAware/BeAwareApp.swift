ab32f0f00bd61cb01bcc3e4a81b938acd89e7c41
HiteshParikh <hiteshparikhus@gmail.com>
2022-02-02 14:10:56 -0500

    Initial Commit

---
+++ BeAware/BeAwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeAwareApp.swift
+//  BeAware
+//
+//  Created by Hitesh Parikh on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BeAwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeAware/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeAware
+//
+//  Created by Hitesh Parikh on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
