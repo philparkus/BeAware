e9b88093b34536004bf099d447cecb28c8de21cc
Saamer Mansoor <i@saamer.me>
2022-02-09 14:08:07 -0500

    Fixed the page UI

--- BeAware/AlertView.swift
+++ BeAware/AlertView.swift
@@ -11,87 +11,83 @@ struct AlertView : View {
     @State private var noiseThreshold: Double = 10
     var body : some View {
         NavigationView{
-            VStack {
-                Spacer ()
-                ZStack {
-                    Color("BrandColor")
-                                .ignoresSafeArea()
-                VStack {
-                    
-                    Text("Noise Length")
-                        .font(Font.custom("Avenir", size: 24))
-                                        .fontWeight(.heavy)
-                                        .padding([.trailing], 210)
-                        .foregroundColor(Color(hex: 0xB2CCDE))
-                    Slider(value: $noiseLength, in: 0...10)
-                        .padding([.top, .leading, .trailing], 30.0)
-                            
-                    HStack {
-                        Text("Short")
-                            .font(Font.custom("Avenir", size: 20))
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color(hex: 0xB2CCDE))
-                        Spacer()
-                            .frame(width: 265)
-                        Text("Long")
-                            .font(Font.custom("Avenir", size: 20))
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color(hex: 0xB2CCDE))
-                    }
+            ZStack {
+                Color("BrandColor")
+                ScrollView{
                     VStack {
-                        Spacer()
-                            .frame(height: 100)
-                    }
-                    Text("Noise Threshold")
-                        .font(Font.custom("Avenir", size: 24))
-                                        .fontWeight(.heavy)
-                                        .padding([.trailing], 190)
-                        .foregroundColor(Color(hex: 0xB2CCDE))
-                    Slider(value: $noiseThreshold, in: 0...10)
-                        .padding([.top, .leading, .trailing], 30.0)
-                            
-                    HStack {
-                        Text("Short")
-                            .font(Font.custom("Avenir", size: 20))
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color(hex: 0xB2CCDE))
-                        Spacer()
-                            .frame(width:265)
-                        Text("Long")
-                            .font(Font.custom("Avenir", size: 20))
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color(hex: 0xB2CCDE))
                         
-                    }
-                    VStack {
-                        Spacer()
-                            .frame(height: 100)
+                        Text("Noise Length")
+                            .font(Font.custom("Avenir", size: 24))
+                            .fontWeight(.heavy)
+                            .padding([.trailing], 210)
+                            .foregroundColor(Color(hex: 0xB2CCDE))
+                        Slider(value: $noiseLength, in: 0...10)
+                            .padding([.top, .leading, .trailing], 30.0)
+                        
+                        HStack {
+                            Text("Short")
+                                .font(Font.custom("Avenir", size: 20))
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color(hex: 0xB2CCDE))
+                                .padding(.leading)
+                            Spacer()
+                            Text("Long")
+                                .font(Font.custom("Avenir", size: 20))
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color(hex: 0xB2CCDE))
+                                .padding(.trailing)
+                        }
+                        Spacer(minLength: 30)
+                        Text("Noise Threshold")
+                            .font(Font.custom("Avenir", size: 24))
+                            .fontWeight(.heavy)
+                            .padding([.trailing], 190)
+                            .foregroundColor(Color(hex: 0xB2CCDE))
+                        Slider(value: $noiseThreshold, in: 0...10)
+                            .padding([.top, .leading, .trailing], 30.0)
+                        
+                        HStack {
+                            Text("Short")
+                                .font(Font.custom("Avenir", size: 20))
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color(hex: 0xB2CCDE))
+                                .padding(.leading)
+                            Spacer()
+                            Text("Long")
+                                .font(Font.custom("Avenir", size: 20))
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color(hex: 0xB2CCDE))
+                                .padding(.trailing)
+                            
+                        }
+                        Spacer(minLength: 60)
                         Button {
-        //                    This button should stop alerts
+                            //                    This button should stop alerts
                             print("Image tapped!")
                         } label: {
                             Image(systemName: "record.circle.fill")
                                 .resizable()
-                                    .frame(width: 75.0, height: 75.0)
+                                .foregroundColor(Color(hex:0xB2CCDE))
+                                .frame(width: 75.0, height: 75.0)
                         }
                         Text("Stop Noise Alert")
                             .font(Font.custom("Avenir", size: 24))
-                                            .fontWeight(.heavy)
+                            .fontWeight(.heavy)
                             .foregroundColor(Color(hex: 0xB2CCDE))
+                        
+                        
+                    }}
+                .padding([.top, .leading, .trailing])
+                .navigationTitle("Alert").navigationBarTitleTextColor(Color("BrandColor")).toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        NavigationLink(
+                            destination: SettingsView()
+                        ) {
+                            Image(systemName: "gear")
+                                .foregroundColor(.blue)
+                        }
+                        
                     }
-                    
-                }
-                }
-                Spacer ()
-            }.navigationTitle("Alert").navigationBarTitleTextColor(Color("BrandColor")).toolbar{
-                ToolbarItem(placement: .navigationBarTrailing){
-                    NavigationLink(
-                        destination: SettingsView()
-                    ) {
-                        Image(systemName: "gear")
-                            .foregroundColor(.blue)
-                    }
-                    
                 }
             }
         }
